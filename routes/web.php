bf73f2993d1bd9e358af3fd91274eff3048c7abd
190103366@stu.sdu.edu.kz <190103366@stu.sdu.edu.kz>
2021-02-21 20:05:54 +0600

    new route contact

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,8 @@ Route::get('/profile', function() {
 Route::get('/about', function() {
     return view('about');
 })->name('about');
+
+Route::get('/contact', function() {
+    return view('contact');
+})->name('contact');
+
