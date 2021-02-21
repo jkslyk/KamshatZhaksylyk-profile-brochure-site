e232b5fa7849041665c4b3a79d2e4ee996afc781
190103366@stu.sdu.edu.kz <190103366@stu.sdu.edu.kz>
2021-02-21 19:49:16 +0600

    delete rotes

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/profile', function() {
-    return view('profile');
-})->name('profile');
-
-Route::get('/about', function() {
-    return view('about');
-})->name('about');
-
-Route::get('/contact', function() {
-    return view('contact');
-})->name('contact');
