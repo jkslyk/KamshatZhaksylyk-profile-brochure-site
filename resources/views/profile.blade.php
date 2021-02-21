568bb8c72d231097edea94fe249452493916ab28
190103366@stu.sdu.edu.kz <190103366@stu.sdu.edu.kz>
2021-02-21 19:32:50 +0600

    initial commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html data-wf-domain="www.iamtamara.design" data-wf-page="6011b0f41f0c923954426063" data-wf-site="5f50efa4420f363f6b5670b2" lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<script type="text/javascript" src="profile.js"></script>
+		<title>I am Kamshat</title>
+		<meta content="width=device-width, initial-scale=1" name="viewport"/>
+		<link href="https://uploads-ssl.webflow.com/5f50efa4420f363f6b5670b2/css/iamtamara.webflow.53e419b21.min.css" rel="stylesheet" type="text/css"/>
+		<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
+		<link href="https://uploads-ssl.webflow.com/5f50efa4420f363f6b5670b2/5fe22bc3e6857d68ff049dcc_favicon.png" rel="shortcut icon" type="image/x-icon"/>
+		<script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-179253318-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	
+	  gtag('config', 'UA-179330396-1');
+	</script>
+	</head>
+	<body class="body">
+		<div data-collapse="medium" data-animation="default" data-duration="400" id="navbar" role="banner" class="navbar-2 w-nav">
+			<div class="c-nav-2 w-container">
+				<a href="{{  route('profile')  }}" aria-current="page" class="brand-2 w-nav-brand w--current">
+					<p style = " margin-top: 30px; font-size: 30px;">I am Kamshat</p>
+				</a>
+			<div role="navigation" class="c-menu-2 w-nav-menu">
+				<a href="{{  route('about')  }}" class="c-menu_nav-link-2 w-nav-link">About</a>
+				<a href="{{  route('contact')  }}" class="c-menu_nav-link-2 alyssa w-nav-link">Contact</a>
+			</div>
+			<div class="menu-button w-nav-button">
+				<p class="c-menu_nav-link-2">Menu</p>
+			</div>
+		</div>
+	</div>
+	
+	
+	<header id="top-bonjour" class="header">
+		<main class="bonjour">
+			<div class="div-block"><p class="bonjour">Sa'lem</p>
+				<p class="tagline-copy">(hello)</p>
+				<p class="tagline">I am Kamshat, designer &amp; brand strategist for people on a mission.<br/>I design ethical websites with a mindful approach.</p>
+				<ol role="list" class="social-media-header w-list-unstyled">
+					<li id="w-node-fa822f9eb3a0-2f9eb39f" class="social-media">
+						<a href="#" class="social-media-icon w-inline-block">
+							<img src="https://uploads-ssl.webflow.com/5f50efa4420f363f6b5670b2/5f5b8f6bafdf0e14742b8f96_iconfinder_147_Gmail_logo_logos_4374025.svg" loading="lazy" alt="Email me" width="23" height="23" class="image-40"/>
+						</a>
+					</li>
+					<li id="w-node-fa822f9eb3a3-2f9eb39f" class="social-media"><a href="#" target="_blank" class="social-media-icon w-inline-block">
+						<img src="https://uploads-ssl.webflow.com/5f50efa4420f363f6b5670b2/5f5b8f6ae4a1a58b9391eb64_iconfinder_345_Twitter_logo_4375241.svg" loading="lazy" alt="My Twitter" width="23" height="23" class="image-40"/>
+					</a>
+				</li>
+				<li id="w-node-fa822f9eb3a6-2f9eb39f" class="social-media">
+					<a href="#" target="_blank" class="social-media-icon w-inline-block">
+						<img src="https://uploads-ssl.webflow.com/5f50efa4420f363f6b5670b2/5f5b8f6bd71475bf72080e52_iconfinder_201_Linkedin_logo_logos_4373981.svg" loading="lazy" alt="My LinkedIn" width="23" height="23" class="image-40"/>
+					</a>
+				</li>
+				<li id="w-node-fa822f9eb3a9-2f9eb39f" class="social-media"><a href="#" target="_blank" class="social-media-icon w-inline-block">
+					<img src="https://uploads-ssl.webflow.com/5f50efa4420f363f6b5670b2/5f5b8f6b7fc89db65bbbe2a9_iconfinder_211_Medium_logo_logos_4373968.svg" loading="lazy" width="23" alt="My Medium" height="23" class="image-40"/>
+				</a>
+			</li>
+			<li id="w-node-fa822f9eb3ac-2f9eb39f" class="social-media">
+				<a href="#" target="_blank" class="social-media-icon w-inline-block">
+					<img src="https://uploads-ssl.webflow.com/5f50efa4420f363f6b5670b2/5f5b8f6be4a1a5843991eb65_iconfinder_99_Dribbble_logo_logos_4374062.svg" loading="lazy" alt="My dribbble" width="23" height="23" class="image-40"/>
+				</a>
+			</li>
+		</ol>
+	</div>
+</main>
+</header>
+<footer id="footer" class="footer">
+	<div class="columns-23 w-row">
+		<div class="column-47 w-col w-col-9">
+			<div class="newsletter">
+				<div class="div-block">
+					<h2 class="h2">The newsletter for people on a mission</h2>
+					<p class="paragraph-center">Every month, receive my UX tips in your inbox and start working towards a more inclusive, accessible and ethical web.<br/></p>
+				</div>
+			</div>
+			<div class="form-block w-form">
+				<form id="email-form" name="email-form" data-name="Email Form" method="post" action="#" class="subscribe-form-flex">
+					<div class="subscribe-form-input-wrapper">
+						<input type="email" class="subscribe-form-input w-input" maxlength="256" name="Subscriber-Email" data-name="Subscriber Email" placeholder="Email" id="Subscriber-Email" required=""/>
+						<input type="submit" value="sign up" data-wait="Please wait..." class="submit-button-4 pink-button left w-button"/>
+					</div>
+				</form>
+				<div class="success-message success-message-2 success-message-3 w-form-done">
+					<div class="paragraph-left-copy">✅ Thank you! Check your inbox to confirm your subscription.</div>
+				</div>
+				<div class="error-message w-form-fail">
+					<div class="paragraph-left-copy">❌ Oops! Is your email address correct?</div>
+				</div>
+			</div>
+		</div>
+		<div class="column-49 w-col w-col-3">
+			<div class="container-2 w-container">
+				<a href="#navbar" class="scroll-to-top w-inline-block">
+					<img src="https://uploads-ssl.webflow.com/5f50efa4420f363f6b5670b2/5fe217301a97174ad5db593a_Back%20to%20top.svg" loading="lazy" width="44" alt="back to top" class="image-43"/>
+				</a>
+			</div>
+		</div>
+	</div>
+</footer>
+
+
+<script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=5f50efa4420f363f6b5670b2" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="https://uploads-ssl.webflow.com/5f50efa4420f363f6b5670b2/js/webflow.8ad52da4e.js" type="text/javascript"></script><!--[if lte IE 9]><script src="//cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif]-->
+</body>
+</html>
